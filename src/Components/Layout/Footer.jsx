eb094bf32684259316245a559910b25278e4b0c5
Matthew Fogarty <mattempyre@gmail.com>
2023-01-13 20:17:58 +0100

    Add footer and favicon

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,3 +1,28 @@
 export const Footer = () => (
-	<footer className='text-center'>Little Lemon &copy; 2022</footer>
-)
+  <footer id='footer'>
+    <div className='container'>
+      <img src='assets/images/Logo.svg' alt='Little Lemon Restaurant Logo' />
+      <ul>
+        <li>Navigation</li>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Reservations</li>
+        <li>Order Online</li>
+        <li>Login</li>
+      </ul>
+      <ul>
+        <li>Contact</li>
+        <li>Address</li>
+        <li>Phone Number</li>
+        <li>Email</li>
+      </ul>
+      <ul>
+        <li>Social Media Links</li>
+        <li>Facebook</li>
+        <li>Instagram</li>
+        <li>Foodgram</li>
+      </ul>
+    </div>
+  </footer>
+);
